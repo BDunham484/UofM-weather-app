4a5ef185fecf041459554bb429fbd5921d1718d7
ThereWolf <bdunham484@gmail.com>
2023-04-13 15:59:20 -0500

    getting there

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,8 +2,51 @@ let submitEl = document.getElementById('submit');
 let searchInputEl = document.getElementById('search-input');
 let searchHistoryEl = document.getElementById('search-history');
 let clearHistoryEl = document.getElementById('clear-history');
+let todaysWeatherEl = document.querySelector('.todaysWeather');
 
 let searchHistory = [];
+let lat;
+let lon;
+
+const renderToday = (data) => {
+
+}
+
+const renderWeather = (data) => {
+    renderToday(data);
+    renderForecast(data);
+}
+
+const getWeather = (location) => {
+    let { lat } = location;
+    let { lon } = location;
+    let city = location.name;
+    
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6bb1b7f8b26934ef2b1028b12a559a0f`
+    
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            renderWeather(data);
+        })
+    
+}
+
+const getCoords = (input) => {
+    let coordsUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=6bb1b7f8b26934ef2b1028b12a559a0f`
+
+    fetch(coordsUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            getWeather(data[0]);
+        })
+
+}
 
 const storeSearch = (input) => {
     searchHistory.push(input);
@@ -23,7 +66,7 @@ const getHistory = () => {
     // console.log('searchHistory: ' + searchHistory)
     for (let i = 0; i < searchHistory.length; i++) {
         let citiesEl = document.createElement('button');
-        citiesEl
+        citiesEl.classList.add('btn')
         citiesEl.textContent = searchHistory[i]
         searchHistoryEl.append(citiesEl);
     }
@@ -36,6 +79,7 @@ const formSubmitHandler = (e) => {
 
     storeSearch(input);
     getHistory();
+    getCoords(input);
     searchInputEl.value = '';
 }
 
