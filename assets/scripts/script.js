0ebff31f2d19d5fe392e059cea27e2a174f5a383
ThereWolf <bdunham484@gmail.com>
2023-04-13 18:25:36 -0500

    stuffs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,12 +9,34 @@ let lat;
 let lon;
 
 const renderToday = (data) => {
+    console.log(data);
+    let currentDate = dayjs().format('MMM D, YYYY')
+    let cityName = document.createElement('div');
+    let temp = document.createElement('div');
+    let humidity = document.createElement('div');
+    let windSpeed = document.createElement('div');
+    let iconId = data.weather[0].icon;
+    let iconUrl = `http://openweathermap.org/img/w/${iconId}.png`
+    let iconImg = document.createElement('img');
 
+    iconImg.setAttribute('src', iconUrl)
+
+    cityName.textContent = data.name
+    temp.textContent = 'Temp: ' + data.main.temp
+    humidity.textContent = 'Humidity: ' + data.main.humidity
+    windSpeed.textContent = 'Wind Speed: ' + data.wind.speed
+
+    todaysWeatherEl.append(currentDate)
+    todaysWeatherEl.append(cityName);
+    todaysWeatherEl.append(iconImg);
+    todaysWeatherEl.append(temp);
+    todaysWeatherEl.append(humidity);
+    todaysWeatherEl.append(windSpeed);
 }
 
 const renderWeather = (data) => {
     renderToday(data);
-    renderForecast(data);
+    // renderForecast(data);
 }
 
 const getWeather = (location) => {
@@ -29,13 +51,14 @@ const getWeather = (location) => {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             renderWeather(data);
         })
     
 }
 
 const getCoords = (input) => {
+    todaysWeatherEl.textContent = '';
+
     let coordsUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=6bb1b7f8b26934ef2b1028b12a559a0f`
 
     fetch(coordsUrl)
@@ -66,7 +89,8 @@ const getHistory = () => {
     // console.log('searchHistory: ' + searchHistory)
     for (let i = 0; i < searchHistory.length; i++) {
         let citiesEl = document.createElement('button');
-        citiesEl.classList.add('btn')
+        // citiesEl.setAttribute('data-city', searchHistory[i])
+        citiesEl.classList.add('btn','mb-3')
         citiesEl.textContent = searchHistory[i]
         searchHistoryEl.append(citiesEl);
     }
@@ -74,6 +98,7 @@ const getHistory = () => {
 
 const formSubmitHandler = (e) => {
     e.preventDefault();
+
     let input = searchInputEl.value.trim();
     // console.log('clicked: ' + input);
 
@@ -95,3 +120,8 @@ getHistory();
 
 submitEl.addEventListener('click', formSubmitHandler);
 clearHistoryEl.addEventListener('click', clearHistoryHandler)
+searchHistoryEl.addEventListener('click', (e) => {
+    let city = e.target.textContent
+    getCoords(city);
+
+})
